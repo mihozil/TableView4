c8e0e42cb8005916500fa3702befeedc3c317194
Apple <Apple@Apples-MacBook-Air.local>
2016-01-11 16:43:35 +0700

    Initial Commit

---
+++ UITableView4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView4
+//
+//  Created by Apple on 1/11/16.
+//  Copyright (c) 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableView4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView4
+//
+//  Created by Apple on 1/11/16.
+//  Copyright (c) 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
